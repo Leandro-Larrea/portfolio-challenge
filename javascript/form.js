b93038f19b05b064103c778fcc1da2f85cead198
unknown <meliodas_17@hotmail.com>
2022-07-31 00:14:13 -0300

    card menu workin on mobile

--- javascript/form.js
+++ javascript/form.js
@@ -2,11 +2,14 @@ let cards = document.querySelectorAll(".projects")
 let button = document.querySelectorAll(".projects__slider-button")
 const projectStyle = document.querySelector(".all-projects").style
 const tagButtons = document.querySelectorAll(".contador__tags-button")
-let actual = 1;
+const mobileButtons = document.querySelectorAll(".button-mobile");
+let actual = 0;
 
+	let transformMark = 0;
 
  
 const x3 = (s)=>{	
+
 	arrayTag = cards;
 	let a = null
 	if(filtredCards.length > 0){
@@ -26,7 +29,7 @@ const x3 = (s)=>{
 	if(document.querySelector(".all-projects").classList.length > 1){
 		document.querySelector(".all-projects").classList.remove("animation-right") ||
 		 document.querySelector(".all-projects").classList.remove("animation-left");
-		void document.querySelector(".all-projects").offsetWidth;
+		void document.querySelector(".main").offsetWidth;
 	
 	}
 	if( actual > a){
@@ -35,8 +38,9 @@ const x3 = (s)=>{
 	else{
 	document.querySelector(".all-projects").classList.add("animation-left")
 }
-	let x = (a * 3) - 1
 	actual = a;
+	let x = (a * 3) - 1
+	transformMark = (x - 2)
 	arrayTag.forEach((a, i) =>{
 	if(i === x || i === x-1 || i === x-2){
 		a.classList.remove("off")
@@ -48,7 +52,41 @@ const x3 = (s)=>{
 })
 	
 }
-console.log(cards)
+
+}
+
+
+function slide1(s){
+	let x = null;
+	if (filtredCards.length > 0) {
+		x = filtredCards
+	}
+	else{
+		x = cards
+	}
+		if(document.querySelector(".all-projects").classList.length > 1){
+		document.querySelector(".all-projects").classList.remove("animation-right") ||
+		 document.querySelector(".all-projects").classList.remove("animation-left");
+		void document.querySelector(".main").offsetWidth;
+	}
+	if(s.target.classList.contains("fa-arrow-left" )&& transformMark !== 0){
+		console.log("aca ta bien ")
+		document.querySelector(".all-projects").classList.add("animation-left")
+		x[transformMark].classList.add("off")
+		x[transformMark - 1].classList.remove("off")
+		transformMark --;
+	}
+		if (s.target.classList.contains("fa-arrow-right") && transformMark < x.length -1){
+			console.log("aca ta bien ")
+			document.querySelector(".all-projects").classList.add("animation-right")
+			x[transformMark].classList.add("off")
+			x[transformMark+1].classList.remove("off")
+			transformMark ++
+		
+			console.log("this one it´s right")
+		}
+
+
 }
 
 
@@ -68,9 +106,18 @@ cards.forEach((a) =>{
 }
 )
 actual = 0;
+transformMark = 0
 filtredCards = array;
-x3();
-console.log(filtredCards)
+if(mode === "desktop"){
+	
+	x3()
+}
+if(mode === "mobile"){
+	
+	mobileTransform();
+}
+
+
 buttonController(filtredCards)
 
 }
@@ -91,8 +138,7 @@ if(buttonAmount > button.length){
 if (buttonAmount < button.length) {
 	buttonDelete()
 }
-console.log(buttonAmount)
-console.log(button.length)
+
 function buttonCreator(){
 	
 	for(i = button.length; i < buttonAmount; i++){
@@ -110,15 +156,91 @@ function buttonDelete(){
 
 	}
 }
-	button = document.querySelectorAll(".projects__slider-button")
+	button = document.querySelectorAll(".projects__slider-button");
 	console.log(button.length)
 }
+
+let mode = null;
+
+const comprobar = (item, type)=>{
+for(i = 0; i < item.length ; i++){
+	if (item[i] === type){
+		return true
+	}
+}
+return false;
+}
+
+function mobileTransform(){
+	console.log("se activo mobiletransform")
+	let x = null
+	if (filtredCards.length > 0){
+		x = filtredCards
+	}
+	else{
+		x = cards;
+	}		
+
+		x.forEach((a, b)=>{
+			if(b === transformMark){
+				a.classList.remove("off")
+			}
+			else{
+				if (comprobar(a.classList,"off") === false) {
+				a.classList.add("off")
+			console.log("adding off")
+				}
+			}
+		})
+	}
+
+	function desktopTransform(){
+		console.log("se activo desktoptransform")
+		let x = null
+	if (filtredCards.length > 0){
+		x = filtredCards;
+	}
+	else{
+		x = cards;
+	}
+	
+	x.forEach((a, b)=>{
+		if(b === transformMark || b === transformMark + 1 || b === transformMark +2){
+				a.classList.remove("off")
+		}
+		else{
+			if(comprobar(a.classList,"off") === false){
+				a.classList.add("off")
+			}
+		}
+		
+})
+}
+const check = () =>{
+	if(window.innerWidth <= 650 && mode !== "mobile"){
+		console.log("mobileMode")
+		mode = "mobile"
+		mobileTransform()
+		
+	}
+	if (window.innerWidth > 650 && mode !== "desktop") {
+		console.log("desktopMode")
+		mode = "desktop"
+		desktopTransform()
+	}
+}
+
+check();
+
+
+window.addEventListener("resize", check)
 buttonController(filtredCards)
-
-
 
 tagButtons.forEach((a) =>{
 	a.addEventListener("focus",filtrar)
+})
+mobileButtons.forEach((a) =>{
+	a.addEventListener("click",slide1)
 })
 
 
@@ -126,4 +248,10 @@ tagButtons.forEach((a) =>{
 que busque dentro del itenContent de cada figcaption de cada carta
 y en base a eso quite o agregue unas u otras.
 now we need our x3 function working with a different array depending on which tag it´s selected
-while using the standard cards array to aply the off class on the other elements*/
+while using the standard cards array to aply the off class on the other elements
+	necesito una funcion que al pasar a mobile recorra el array, mantenga el primer
+elemento sin la clase off y al resto le agregue la clase off (de forma que siempre nos paremos
+en el mismo punto del array. Asi mismo necesito una funcion que al pasar a desktop recorra el array que se este usando
+ya sea cards o filtred cards y al encontrar un elemento sin la clase off se asegure de que ese elemento y los siguientes
+ dos (si existen) se queden sin la clase off)*/
+
